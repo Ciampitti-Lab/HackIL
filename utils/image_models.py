5cf99df8f99d2e1f5657ef0f5950f68c3390fe79
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2026-03-07 02:19:05 -0500

    chore: training/evaluating/moving images

--- utils/image_models.py
+++ utils/image_models.py
@@ -18,3 +18,21 @@ class MobileViT(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+class MobileNetV3(nn.Module):
+    def __init__(
+        self,
+        model_name="mobilenetv3_small_100",
+        num_classes=1
+    ):
+        super().__init__()
+
+        self.model = timm.create_model(
+            model_name,
+            pretrained=True,
+            in_chans=3,
+            num_classes=num_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
