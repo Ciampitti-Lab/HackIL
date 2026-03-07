862fa9046f0190b8b88adb704ca19fb1bdc45cb8
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2026-03-06 23:50:18 -0500

    chore: finding the best hyperparameters

---
+++ utils/image_models.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import timm
+
+class MobileViT(nn.Module):
+    def __init__(
+        self,
+        model_name="mobilevit_s",
+        num_classes=1
+    ):
+        super().__init__()
+
+        self.model = timm.create_model(
+            model_name,
+            pretrained=True,
+            in_chans=3,
+            num_classes=num_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
